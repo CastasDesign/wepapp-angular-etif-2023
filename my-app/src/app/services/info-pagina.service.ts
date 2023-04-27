8159cf64ad8f32a9f1071931cbe015257ca34c3c
Dsen6301 <125303601+CastasDesign@users.noreply.github.com>
2023-04-27 17:55:43 +0200

    27/04/23

--- my-app/src/app/services/info-pagina.service.ts
+++ my-app/src/app/services/info-pagina.service.ts
@@ -6,17 +6,32 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
   providedIn: 'root',
 })
 export class InfoPaginaService {
-  info: InfoPagina={};
+  info: InfoPagina = {};
+  equipo: any = [];
 
   constructor(private http: HttpClient) {
     // console.log("Servicio de infoPagina listo")
-
-    // Leer el archivo JSON
-    this.http
-    .get('assets/data/data-pagina.json')
-    .subscribe((resp: any) => {
-      this.info = resp; // provar resp. I veurem les propietats JSON
-      console.log(resp);
-    });
+    this.cargarInfo();
+    this.cargarEquipo();
   }
+  // Leer el archivo JSON interno
+  private cargarInfo() {
+    this.http
+      .get('assets/data/data-pagina.json')
+      .subscribe((resp: InfoPagina) => {
+        this.info = resp; // provar resp. I veurem les propietats JSON
+        console.log(resp);
+      });
+  }
+  // Leer el archivo JSON externo en la Real Time DataBase de Firebase de Google
+  private cargarEquipo() {
+    this.http
+      .get('https://web-app-etif-angular-2023-default-rtdb.europe-west1.firebasedatabase.app/equipo.json')
+      .subscribe((resp: InfoPagina) => {
+        this.equipo = resp; // provar resp. I veurem les propietats JSON
+        console.log(resp);
+      });
+  }
+  
+  
 }
